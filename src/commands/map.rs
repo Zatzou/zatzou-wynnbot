7ed7c65043a82c4a21343d2362bc03d384f5040c
Zatzou <12207527+Zatzou@users.noreply.github.com>
2021-11-04 01:12:27 +0200

    switch to image + imageproc from resvg

--- src/commands/map.rs
+++ src/commands/map.rs
@@ -1,16 +1,21 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
 use std::error::Error;
-use std::rc::Rc;
 
 use crc32fast::Hasher;
+use image::png::PngEncoder;
+use image::{ColorType, DynamicImage, ImageBuffer, Rgba};
+use imageproc::drawing;
+use imageproc::drawing::Canvas;
+use imageproc::rect::Rect;
+use rusttype::{Font, Scale};
 use once_cell::sync::OnceCell;
 use serenity::framework::standard::{macros::command, CommandResult};
 use serenity::http::AttachmentType;
 use serenity::model::prelude::*;
 use serenity::prelude::*;
-use tiny_skia::Pixmap;
-use usvg::{NodeExt, Tree};
+
+use image::io::Reader as ImageReader;
 
 use tracing::{error, info};
 
@@ -22,14 +27,18 @@ use crate::{BOT_NAME, BOT_VERSION};
 use cached::proc_macro::cached;
 
 /// Static for the image file so we don't have to load it every time
-static MAPBASE: OnceCell<Pixmap> = OnceCell::new();
+static MAPBASE: OnceCell<image::ImageBuffer<Rgba<u8>, Vec<u8>>> = OnceCell::new();
 
 /// Helper function for getting the base map
-fn get_mapbase() -> Result<Pixmap, Box<dyn Error + Send + Sync>> {
+fn get_mapbase() -> Result<image::ImageBuffer<Rgba<u8>, Vec<u8>>, Box<dyn Error + Send + Sync>> {
     let out = if let Some(pm) = MAPBASE.get() {
         pm.clone()
     } else {
-        let map = tiny_skia::Pixmap::load_png("./main-map2.png")?;
+        let map: image::ImageBuffer<Rgba<u8>, Vec<u8>> = if let DynamicImage::ImageRgba8(img) = ImageReader::open("./main-map2.png")?.decode()? {
+            img
+        } else {
+            panic!("main-map2.png invalid!!!");
+        };
         MAPBASE.set(map.clone()).unwrap();
         map
     };
@@ -53,146 +62,80 @@ async fn map(ctx: &Context, msg: &Message) -> CommandResult {
     let terrs = get_map().await?;
 
     // ouput image
-    let mut out = get_mapbase()?;
+    let mut out = drawing::Blend(get_mapbase()?);
 
-    // all of this rendering stuff has to be in a closure because the ownership and async don't work otherwise
-    {
-        // create base svg
-        let svg = usvg::Svg {
-            size: usvg::Size::new(out.width() as f64, out.height() as f64).unwrap(),
-            view_box: usvg::ViewBox {
-                rect: usvg::Rect::new(0.0, 0.0, out.width() as f64, out.height() as f64).unwrap(),
-                aspect: usvg::AspectRatio::default(),
-            },
+    // go thru all territories and render the rects for them
+    for (_, terr) in terrs.territories.iter() {
+        let loc = &terr.location;
+
+        // widths
+        let width = (f64::abs(loc.endX - loc.startX) / 3.0) as u32;
+        let height = (f64::abs(loc.endZ - loc.startZ) / 3.0) as u32;
+
+        // position calculations
+        let x = if loc.startX < loc.endX {
+            calc_x(loc.startX) as i32
+        } else {
+            calc_x(loc.endX) as i32
         };
-        let svgtree = usvg::Tree::create(svg);
+        let y = if loc.startZ < loc.endZ {
+            calc_z(loc.startZ) as i32
+        } else {
+            calc_z(loc.endZ) as i32
+        };
 
-        // go thru all territories and render the rects for them
-        for (_, terr) in terrs.territories.iter() {
-            let loc = &terr.location;
+        // guild color calculations
+        let col = if !terr.guildColor.is_empty() {
+            terr.guildColor[1..].to_owned()
+        } else {
+            hex::encode(guild_color(terr.guild.clone()).to_ne_bytes())[0..=5].to_owned()
+        };
 
-            // widths
-            let width = f64::abs(loc.endX - loc.startX) / 3.0;
-            let height = f64::abs(loc.endZ - loc.startZ) / 3.0;
+        // hex to rgb
+        let color = colorsys::Rgb::from_hex_str(&col)?;
 
-            // position calculations
-            let x = if loc.startX < loc.endX {
-                calc_x(loc.startX)
-            } else {
-                calc_x(loc.endX)
-            };
-            let y = if loc.startZ < loc.endZ {
-                calc_z(loc.startZ)
-            } else {
-                calc_z(loc.endZ)
-            };
+        let fillcol = Rgba([color.red() as u8, color.green() as u8, color.blue() as u8, 127]);
+        let edgecol = Rgba([color.red() as u8, color.green() as u8, color.blue() as u8, 255]);     
 
-            // guild color calculations
-            let col = if !terr.guildColor.is_empty() {
-                terr.guildColor[1..].to_owned()
-            } else {
-                hex::encode(guild_color(terr.guild.clone()).to_ne_bytes())[0..=5].to_owned()
-            };
+        let area = Rect::at(x, y).of_size(width, height);
 
-            // hex to rgb
-            let color = colorsys::Rgb::from_hex_str(&col)?;
+        drawing::draw_filled_rect_mut(&mut out, area, fillcol);
+        drawing::draw_hollow_rect_mut(&mut out, area, edgecol);
+    }
 
-            // usvg stuff
-            let stroke = Some(usvg::Stroke {
-                paint: usvg::Paint::Color(usvg::Color::new_rgb(
-                    color.red() as u8,
-                    color.green() as u8,
-                    color.blue() as u8,
-                )),
-                opacity: 1.0.into(),
-                width: 2.5.into(),
-                ..usvg::Stroke::default()
-            });
+    let mut guilds: HashMap<String, Vec<Territory>> = HashMap::new();
 
-            let fill = Some(usvg::Fill {
-                paint: usvg::Paint::Color(usvg::Color::new_rgb(
-                    color.red() as u8,
-                    color.green() as u8,
-                    color.blue() as u8,
-                )),
-                opacity: 0.5.into(),
-                ..usvg::Fill::default()
-            });
+    for (_name, terr) in terrs.territories.into_iter() {
+        if let Some(t) = guilds.get_mut(&terr.guildPrefix) {
+            t.push(terr);
+        } else {
+            let guild = terr.guildPrefix.clone();
+            let vec: Vec<Territory> = Vec::from([terr]);
+            guilds.insert(guild, vec);
+        };
+    }
 
-            // create the element
-            svgtree.root().append_kind(usvg::NodeKind::Path(usvg::Path {
-                stroke,
-                fill,
-                data: Rc::new(usvg::PathData::from_rect(usvg::Rect::new(
-                    x, y, width, height,
-                ).unwrap())),
-                ..usvg::Path::default()
-            }));
-        }
+    // name rendering
+    let font_data: &[u8] = include_bytes!("../../Roboto-Bold.ttf");
+    let font: Font<'static> = Font::try_from_bytes(font_data).unwrap();
+    
+    let col = Rgba([255, 255, 255, 255]);
+    for (name, terrs) in guilds.iter() {
+        for area in terrs {
+            let loc = &area.location;
+            let x_start = calc_x(loc.startX).min(calc_x(loc.endX));
+            let y_start = calc_z(loc.startZ).min(calc_z(loc.endZ));
 
-        let mut guilds: HashMap<String, Vec<Territory>> = HashMap::new();
-
-        for (_name, terr) in terrs.territories.into_iter() {
-            if let Some(t) = guilds.get_mut(&terr.guildPrefix) {
-                t.push(terr);
-            } else {
-                let guild = terr.guildPrefix.clone();
-                let vec: Vec<Territory> = Vec::from([terr]);
-                guilds.insert(guild, vec);
-            };
-        }
-
-        let mut names: HashMap<String, (f64, f64)> = HashMap::new();
-
-        for (name, terrs) in guilds.iter() {
-            let mut avg_x: Vec<f64> = Vec::new();
-            let mut avg_z: Vec<f64> = Vec::new();
-
-            for area in terrs {
-                let loc = &area.location;
-                let x_start = if loc.startX < loc.endX {
-                    (loc.startX / 3.0) - 566.333 + 1364.0
-                } else {
-                    (loc.endX / 3.0) - 566.333 + 1364.0
-                };
-                let y_start = if loc.startZ < loc.endZ {
-                    (loc.startZ / 3.0) + 41.0 + 2162.0
-                } else {
-                    (loc.endZ / 3.0) + 41.0 + 2162.0
-                };
-                let x_end = if loc.startX > loc.endX {
-                    (loc.startX / 3.0) - 566.333 + 1364.0
-                } else {
-                    (loc.endX / 3.0) - 566.333 + 1364.0
-                };
-                let y_end = if loc.startZ > loc.endZ {
-                    (loc.startZ / 3.0) + 41.0 + 2162.0
-                } else {
-                    (loc.endZ / 3.0) + 41.0 + 2162.0
-                };
-                avg_x.push(x_end - x_start);
-                avg_z.push(y_end - y_start);
-            }
-
-            names.insert(name.to_string(), (median(&avg_x), median(&avg_z)));
-        }
-
-        // render out the svg
-        resvg::render(&svgtree, usvg::FitTo::Original, out.as_mut());
-
-        // name stuff TODO: not worki
-        for (name, pos) in names {
-            let svege = usvg::Tree::from_str(&format!(r#"<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1364 2162"><text x="{}" y="{}">{}</text></svg>"#, pos.0, pos.1, name), &usvg::Options::default().to_ref())?;
-            //info!("{} at {} {}", name, pos.0, pos.1);
-            resvg::render(&svege, usvg::FitTo::Original, out.as_mut());
+            drawing::draw_text_mut(&mut out, col, x_start as u32 + 3, y_start as u32 + 3, Scale::uniform(10.0), &font, &name);
         }
     }
 
-    //let opt: Options = Options::default();
-    //resvg::render(&usvg::Tree::from_data(&std::fs::read("./map.svg").unwrap(), &opt.to_ref()).unwrap(), usvg::FitTo::Original, out.as_mut());
+    // encode png
+    let mut png_data: Vec<u8> = Vec::new();
 
-    // get the png
-    let png_data = out.encode_png()?;
+    let encoder = PngEncoder::new(&mut png_data);
+    let size = out.dimensions();
+    encoder.encode(&out.0, size.0, size.1, ColorType::Rgba8)?;
 
     // serenity wants a cow for whatever reason
     let cow = Cow::from(png_data);
@@ -242,21 +185,6 @@ fn calc_z(z: f64) -> f64 {
     (z / 3.0) + 41.0 + 2162.0
 }
 
-fn median(list: &[f64]) -> f64 {
-    let len = list.len();
-    let mid = len / 2;
-    if len % 2 == 0 {
-        mean(&list[(mid - 1)..(mid + 1)])
-    } else {
-        list[mid]
-    }
-}
-
-fn mean(list: &[f64]) -> f64 {
-    let sum: f64 = Iterator::sum(list.iter());
-    sum / (list.len() as f64)
-}
-
 #[command]
 async fn gather(ctx: &Context, msg: &Message) -> CommandResult {
     let cmd_args = parse_command_args(msg);
@@ -287,59 +215,49 @@ async fn gather(ctx: &Context, msg: &Message) -> CommandResult {
 
     let spots = Gather::get_gatherspots().await?;
 
-    let mut out = get_mapbase()?;
+    let mut out = drawing::Blend(get_mapbase()?);
 
     let mut count = 0;
-    {
-        // create base svg
-        let svg = usvg::Svg {
-            size: usvg::Size::new(out.width() as f64, out.height() as f64).unwrap(),
-            view_box: usvg::ViewBox {
-                rect: usvg::Rect::new(0.0, 0.0, out.width() as f64, out.height() as f64).unwrap(),
-                aspect: usvg::AspectRatio::default(),
-            },
-        };
-        let mut svgtree = usvg::Tree::create(svg);
 
-        if types.contains("W") {
-            for spot in spots.woodCutting {
-                if spot.r#type == wanted {
-                    count += 1;
-                    add_rect(spot, &mut svgtree);
-                }
+    if types.contains("W") {
+        for spot in spots.woodCutting {
+            if spot.r#type == wanted {
+                count += 1;
+                add_rect(spot, &mut out);
             }
         }
-        if types.contains("M") {
-            for spot in spots.mining {
-                if spot.r#type == wanted {
-                    count += 1;
-                    add_rect(spot, &mut svgtree);
-                }
+    }
+    if types.contains("M") {
+        for spot in spots.mining {
+            if spot.r#type == wanted {
+                count += 1;
+                add_rect(spot, &mut out);
             }
         }
-        if types.contains("G") {
-            for spot in spots.farming {
-                if spot.r#type == wanted {
-                    count += 1;
-                    add_rect(spot, &mut svgtree);
-                }
+    }
+    if types.contains("G") {
+        for spot in spots.farming {
+            if spot.r#type == wanted {
+                count += 1;
+                add_rect(spot, &mut out);
             }
         }
-        if types.contains("F") {
-            for spot in spots.fishing {
-                if spot.r#type == wanted {
-                    count += 1;
-                    add_rect(spot, &mut svgtree);
-                }
+    }
+    if types.contains("F") {
+        for spot in spots.fishing {
+            if spot.r#type == wanted {
+                count += 1;
+                add_rect(spot, &mut out);
             }
         }
-
-        // render svg
-        resvg::render(&svgtree, usvg::FitTo::Original, out.as_mut());
     }
 
-    // get the png
-    let png_data = out.encode_png()?;
+    // encode png
+    let mut png_data: Vec<u8> = Vec::new();
+
+    let encoder = PngEncoder::new(&mut png_data);
+    let size = out.dimensions();
+    encoder.encode(&out.0, size.0, size.1, ColorType::Rgba8)?;
 
     // serenity wants a cow for whatever reason
     let cow = Cow::from(png_data);
@@ -380,28 +298,16 @@ async fn gather(ctx: &Context, msg: &Message) -> CommandResult {
     Ok(())
 }
 
-fn add_rect(spot: GatherSpot, svgtree: &mut Tree) {
-    let x = calc_x(spot.location.x);
-    let y = calc_z(spot.location.z);
+const SPOTCOL: Rgba<u8> = Rgba([255, 0, 0, 255]);
 
-    let fill = Some(usvg::Fill {
-        paint: usvg::Paint::Color(usvg::Color::new_rgb(
-            255,
-            0,
-            0,
-        )),
-        opacity: 1.0.into(),
-        ..usvg::Fill::default()
-    });
+fn add_rect(spot: GatherSpot, img: &mut drawing::Blend<ImageBuffer<Rgba<u8>, Vec<u8>>>) {
+    let x = calc_x(spot.location.x) as i32;
+    let y = calc_z(spot.location.z) as i32;
+
+    let rect = Rect::at(x - 2, y - 2).of_size(4, 4);
 
     // create the element
-    svgtree.root().append_kind(usvg::NodeKind::Path(usvg::Path {
-        fill,
-        data: Rc::new(usvg::PathData::from_rect(usvg::Rect::new(
-            x - 2.5, y - 2.5, 5.0, 5.0,
-        ).unwrap())),
-        ..usvg::Path::default()
-    }));
+    drawing::draw_filled_rect_mut(img, rect, SPOTCOL);
 }
 
 async fn gather_usage(ctx: &Context, msg: &Message) -> CommandResult {
